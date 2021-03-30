961db5d449aaeca8dd9dbd2c6f2340ed5efbeccd
Miaobing <mjiang@forzadata.com>
2021-03-30 18:45:58 +0800

    add log4j2 support.

--- grpc-client/src/main/java/com/demo/grpcclient/GrpcClientApplication.java
+++ grpc-client/src/main/java/com/demo/grpcclient/GrpcClientApplication.java
@@ -1,13 +1,26 @@
 package com.demo.grpcclient;
 
+import brave.Tracing;
+import brave.context.log4j2.ThreadContextScopeDecorator;
+import brave.propagation.ThreadLocalCurrentTraceContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GrpcClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(GrpcClientApplication.class, args);
+    }
+
+    @Bean
+    public Tracing tracing() {
+        return Tracing.newBuilder()
+                .currentTraceContext(ThreadLocalCurrentTraceContext.newBuilder()
+                        .addScopeDecorator(ThreadContextScopeDecorator.get())
+                        .build()
+                ).build();
     }
 }
--- provider/src/main/java/com/example/provider/controller/HelloController.java
+++ provider/src/main/java/com/example/provider/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.provider.controller;
 
 import com.example.provider.proxy.GrpcClientService;
+import org.apache.logging.log4j.ThreadContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/hello")
 public class HelloController {
+    private Logger logger= LoggerFactory.getLogger("providerLogger");
     @Autowired
     GrpcClientService grpcClientService;
     @RequestMapping(value = "")
@@ -18,11 +22,13 @@ public class HelloController {
 
     @RequestMapping(value = "callgrpc")
     public Object callGrpc(@RequestParam(value = "name")String name){
+        logger.info("call rpc");
         return grpcClientService.hello(name);
     }
 
     @RequestMapping(value = "callgrpc2")
     public Object callGrpc2(@RequestParam(value = "name")String name){
+        logger.info("call rpc2");
         return grpcClientService.hello2(name);
     }
 }
