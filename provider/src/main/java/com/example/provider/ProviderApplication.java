b1188ab0e3dab200974bc467401694712010a6f8
Miaobing <mjiang@forzadata.com>
2021-04-01 19:33:50 +0800

    use kafka as sender.

--- provider/src/main/java/com/example/provider/ProviderApplication.java
+++ provider/src/main/java/com/example/provider/ProviderApplication.java
@@ -1,13 +1,9 @@
 package com.example.provider;
 
-import brave.Tracing;
-import brave.context.log4j2.ThreadContextScopeDecorator;
-import brave.propagation.ThreadLocalCurrentTraceContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -16,14 +12,5 @@ public class ProviderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class, args);
-    }
-
-    @Bean
-    public Tracing tracing() {
-        return Tracing.newBuilder()
-                .currentTraceContext(ThreadLocalCurrentTraceContext.newBuilder()
-                        .addScopeDecorator(ThreadContextScopeDecorator.get())
-                        .build()
-                ).build();
     }
 }
